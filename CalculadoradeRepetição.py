9fe5737e769a6e1a6b1b88a0000feac94c31d1d4
unknown <arielmmnogueira@gmail.com>
2023-04-20 14:01:28 -0300

    Neste commit fiz o codigo para o uso das listas para a conversão e mudei a estrutura das impressões para maior entendimento do codigos e para poder entender quais os pedidos o programa estava fazendo

--- CalculadoradeRepetição.py
+++ CalculadoradeRepetição.py
@@ -1,19 +1,27 @@
 medidas = ["bit", "byte", "kilobyte", "megabyte", "gigabyte","terabyte","petabyte"]
 
-print("Escolha a unidade de medida:(Gigabyte):")
-unid1 = input()
-print(unid1)
-
-print("Escolha a unidade de medida:(Terabyte):")
-unid2 = input()
-print(unid2)
-
-def conversor_medidas (numero,unidOrigem,unidSaida):
-    indice_Origem = medidas.index(unidOrigem)
-    indice_saida = medidas.index(unidSaida)
+def conversor_medidas (numero,unidorigem,unidsaida):
+    indice_Origem = medidas.index(unidorigem)
+    indice_saida = medidas.index(unidsaida)
     fator = 1024** (indice_saida - indice_Origem)
     return numero * fator
 
-valor = input("Escolha o Valor a ser convertido:")
+valor = int(input("Escolha o Valor a ser convertido:"))
 
+unidorigem = input("Escolha a unidade de medida:(Gigabyte):")
 
+while unidorigem not in medidas :
+    print("Unidade não existente, tente novamente!: ")
+    unid= input()
+
+unidsaida = input("Escolha a unidade de medida:(Terabyte):")
+while unidsaida not in medidas :
+    print("Unidade não existente, tente novamente!: ")
+    unid= input()
+
+while medidas.index(unidorigem) < medidas.index(unidsaida):
+    nova_unidade = medidas[medidas.index(unidorigem) + 1]
+    quantidade = conversor_medidas(quantidade, unidorigem, nova_unidade)
+    unidade_origem = nova_unidade
+
+    print("O resultado é:", quantidade, unidade_origem)
